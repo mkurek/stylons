b9dd016de0a36b41f2ec73bd0c78784c677df850
mateusz dembski <mateusz.dembski@gmail.com>
2011-09-22 23:32:30 +0200

    Komentarze w senderze, oprorgamowany przycisk anuluj.

--- src/pysencha/public/lib/pysencha/sender.js
+++ src/pysencha/public/lib/pysencha/sender.js
@@ -1,4 +1,14 @@
 var Sender = {
+	
+		/**
+		 * @public Return JSON object
+		 * 
+		 * @param {object}
+		 *            url string with url
+		 * 
+		 * @return {object} JSON object
+		 */
+		
 	getFromURL: function(url) {
 		var response , xhr = new XMLHttpRequest();
 		xhr.open("POST",url,false);
@@ -11,6 +21,17 @@ var Sender = {
 		}
 		return response;
 	},
+	
+		/**
+		 * @public Return JSON object
+		 * @param {object}
+		 *            url string with url
+		 * 
+		 * @param {object}
+		 *            data Object with data from Form
+		 * @return {object} JSON object
+		 */
+	
 	sendToURL: function(url, data) {
 		var response, dataForServer, xhr = new XMLHttpRequest();
 		xhr.open("POST",url,false);
