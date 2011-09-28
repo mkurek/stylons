c2e91b3612733b3f8e7aa5fcb9f766d5aabd5394
kubked <devnull@localhost>
2011-09-28 13:49:42 +0200

    bezpieczniejszy handler

--- src/pysencha/controllers/handler.py
+++ src/pysencha/controllers/handler.py
@@ -6,15 +6,13 @@ from pylons.controllers.util import abort, redirect
 from pysencha.lib.base import BaseController, render
 
 log = logging.getLogger(__name__)
+testpath = 'testy/refapp/'
 
 class HandlerController(BaseController):
     def load(self, url):
-        # Return a rendered template
-        #return render('/handler.mako')
-        # or, return a response
-        print "Tutaj : " + url
-        pathname = os.path.dirname(os.path.dirname(sys.argv[0]))
-        f = open(str(pathname+'/src/pysencha/'+url))
+        serverpath = os.path.dirname(os.path.dirname(__file__))
+        path = os.path.join(serverpath, testpath, url)
+        f = open(path)
         plik  = f.read()
         f.close()
         return plik
